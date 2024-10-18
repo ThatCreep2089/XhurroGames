fdd0dcc99588f11ffcf3ebf184e139fa91ee1942
Andrea G.A. <andrgo37@ucm.es>
2024-10-18 18:35:09 +0200

    importado el canvas a la web

---
+++ src/test.js
@@ -0,0 +1,26 @@
+export default class Test extends Phaser.Scene
+{
+    constructor()
+    {
+        super({key: "test"});
+    }
+
+
+    init(){
+
+    }
+
+    preload()
+    {
+
+    }
+
+    create(){
+
+    }
+
+    update(){
+        
+    }
+
+}
