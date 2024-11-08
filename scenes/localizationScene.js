2c620bcc36c701c584166aef2c7c21a6e7658661
Andrea G.A. <andrgo37@ucm.es>
2024-11-08 18:47:18 +0100

    localizationScene en pañales

--- scenes/localizationScene.js
+++ scenes/localizationScene.js
@@ -9,32 +9,78 @@ export default class localizationScene extends Phaser.Scene
     }
 
     preload(){
-        this.load.image('parque', 'assets/parque.jpg'); //fondo
-        this.load.image('puente', 'assets/puente.jpg'); //fondo
-        this.load.image('bar', 'assets/barFondo.jpg'); //fondo
-        this.load.image('cni', 'assets/cniFondo.jpg'); //fondo
-        this.load.image('hipodromo', 'assets/hipodromoFondo.jpg'); //fondo
+        //FONDOS
+            this.load.image('parque', 'assets/parque.jpg'); //fondo
+            this.load.image('puente', 'assets/puente.jpg'); //fondo
+            this.load.image('bar', 'assets/barFondo.jpg'); //fondo
+            this.load.image('cni', 'assets/cniFondo.jpg'); //fondo
+            this.load.image('hipodromo', 'assets/hipodromoFondo.jpg'); //fondo
+
+        //NPCS
+
+        //FLECHA
+
+        //OBJETOS COLECCIONABLES
+        
     }
 
     create(data){
-        // Usar el parámetro 'fondo' para decidir qué fondo cargar
-        const fondo = data.fondo || 'parque';
         
-        //Pintamos un fondo
-        var back = this.add.image(0, 0, fondo).setOrigin(0, 0);
+        //1. PINTAR FONDO
+            // Usar el parámetro 'fondo' para decidir qué fondo cargar
+            const fondo = data.fondo || 'parque';
+            
+            //Pintamos un fondo
+            var back = this.add.image(0, 0, fondo).setOrigin(0, 0);
 
-        //escalar el fondo
-        var scaleX = this.cameras.main.width / back.width;
-        var scaleY = this.cameras.main.height / back.height;
-        
-        var scale = Math.min(scaleX, scaleY);
-        
-        back.setScale(scale);
-        
-        back.setPosition(
-            this.cameras.main.width / 2 - back.displayWidth / 2,
-            this.cameras.main.height / 2 - back.displayHeight / 2
-        );
+            //escalar el fondo
+            var scaleX = this.cameras.main.width / back.width;
+            var scaleY = this.cameras.main.height / back.height;
+            
+            var scale = Math.min(scaleX, scaleY);
+            
+            back.setScale(scale);
+            
+            back.setPosition(
+                this.cameras.main.width / 2 - back.displayWidth / 2,
+                this.cameras.main.height / 2 - back.displayHeight / 2
+            );
+
+        //2. AÑADIR ELEMENTOS LOCALIZACIÓN
+            //elle NO visible
+            //frase
+            //3 botones NPC (depende de data)
+            //Flechas
+            //agrupación objetos coleccionables (depende de data)
+            //boton back
+            //ansiedad
+            //botón aceptar
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         // botones para testeo
