89bb0dc5d8bc2ee3a74e266859eccd419063fceb
Andrea G.A. <149385951+andreaGonzAnas@users.noreply.github.com>
2024-11-09 12:56:57 +0100

    localization scene
    mas de la escena, cambios dependientes de cual localizacion

--- scenes/localizationScene.js
+++ scenes/localizationScene.js
@@ -6,6 +6,7 @@ export default class localizationScene extends Phaser.Scene
         var acceptButton, backButton;
         var show;
         this.mode;
+        this.titulo = null;
     }
 
     init(){
@@ -42,10 +43,10 @@ export default class localizationScene extends Phaser.Scene
         
         //1. PINTAR FONDO
             // Usar el parámetro 'fondo' para decidir qué fondo cargar
-            const fondo = data.fondo || 'parque';
+            this.mode = data.fondo || 'parque';
             
             //Pintamos un fondo
-            var back = this.add.image(0, 0, fondo).setOrigin(0, 0);
+            var back = this.add.image(0, 0, this.mode).setOrigin(0, 0);
 
             //escalar el fondo
             var scaleX = this.cameras.main.width / back.width;
@@ -61,16 +62,15 @@ export default class localizationScene extends Phaser.Scene
             );
 
         //2. AÑADIR ELEMENTOS LOCALIZACIÓN
-            //3 botones NPC (depende de data)
             //agrupación objetos coleccionables (depende de data)
             //ansiedad
             //elle NO visible
 
 
             // TEXTO
-            let titulo = this.add.text(
+            this.titulo = this.add.text(
                 this.sys.game.canvas.width / 2,   // Coordenada X: centrado horizontalmente
-                this.sys.game.canvas.height / 10, // Coordenada Y: 1/10 del alto de la pantalla 
+                this.sys.game.canvas.height / 12, // Coordenada Y: 1/10 del alto de la pantalla 
                 "Con quien quieres hablar?", 
                 { 
                     fontSize: '100px', 
@@ -78,25 +78,241 @@ export default class localizationScene extends Phaser.Scene
                     fontFamily: 'Georgia',  
                 }
             );
-            titulo.setStroke('#000000', 8);  // Trazo negro, puedes ajustar el grosor o eliminarlo
-            titulo.setOrigin(0.5, 0);
+            this.titulo.setStroke('#000000', 8);  // Trazo negro, puedes ajustar el grosor o eliminarlo
+            this.titulo.setOrigin(0.5, 0);
+            this.titulo.setScale(0.8);
 
             
+            //NPCS (DEPENDEN DE DATA)
+            if(this.mode == 'bar')
+                {
+                    var names = this.add.group();
+
+                        let pacoName = this.add.text(
+                            this.sys.game.canvas.width / 4,   // Coordenada X: centrado horizontalmente
+                            this.sys.game.canvas.height / 3.7, // Coordenada Y: 1/10 del alto de la pantalla 
+                            "PACO", 
+                            { 
+                                fontSize: '100px', 
+                                color: '#999999',       // Negro
+                                fontFamily: 'Georgia',  
+                            }
+                        );
+                        pacoName.setStroke('#000000', 8);  // Trazo negro, puedes ajustar el grosor o eliminarlo
+                        pacoName.setOrigin(0.5, 0);
+                        pacoName.setScale(0.6);
+
+                        names.add(pacoName);
+                    
+                        //PACO
+                        const paco = this.add.image(
+                            this.sys.game.canvas.width / 4,
+                            this.sys.game.canvas.height / 1.4, 
+                            'paco')
+                        .setOrigin(0.5, 0.5)
+                        .setScale(0.9)
+                        .setInteractive()
+                        .on('pointerdown', () => this.acceptButton(true, arrows, names, acceptButton, backButton, "Quieres hablar con PACO?"))
+                        .on('pointerover', () => paco.setTint(0xff0000))
+                        .on('pointerout', () => paco.clearTint());
+
+                    
+                }
+                else if(this.mode == 'cni')
+                {
+                    var names = this.add.group();
+
+                        
+                        let mariaName = this.add.text(
+                            this.sys.game.canvas.width / 1.37,   // Coordenada X: centrado horizontalmente
+                            this.sys.game.canvas.height / 3.7, // Coordenada Y: 1/10 del alto de la pantalla 
+                            "MARIA", 
+                            { 
+                                fontSize: '100px', 
+                                color: '#999999',       // Negro
+                                fontFamily: 'Georgia',  
+                            }
+                        );
+                        mariaName.setStroke('#000000', 8);  // Trazo negro, puedes ajustar el grosor o eliminarlo
+                        mariaName.setOrigin(0.5, 0);
+                        mariaName.setScale(0.6);
+
+                        names.add(mariaName);
+                    
+                        //MARIA
+                        const maria = this.add.image(
+                            this.sys.game.canvas.width / 1.35,
+                            this.sys.game.canvas.height / 1.4, 
+                            'maria')
+                        .setOrigin(0.5, 0.5)
+                        .setScale(0.6)
+                        .setInteractive()
+                        .on('pointerdown', () => this.acceptButton(true, arrows, names, acceptButton, backButton, "Quieres hablar con MARIA?"))
+                        .on('pointerover', () => maria.setTint(0xff0000))
+                        .on('pointerout', () => maria.clearTint());
+                }
+                else if(this.mode == 'hipodromo')
+                {
+                    var names = this.add.group();
+
+                        let humbertoName = this.add.text(
+                            this.sys.game.canvas.width / 2,   // Coordenada X: centrado horizontalmente
+                            this.sys.game.canvas.height / 3.7, // Coordenada Y: 1/10 del alto de la pantalla 
+                            "HUMBERTO", 
+                            { 
+                                fontSize: '100px', 
+                                color: '#999999',       // Negro
+                                fontFamily: 'Georgia',  
+                            }
+                        );
+                        humbertoName.setStroke('#000000', 8);  // Trazo negro, puedes ajustar el grosor o eliminarlo
+                        humbertoName.setOrigin(0.5, 0);
+                        humbertoName.setScale(0.6);
+
+                        names.add(humbertoName);
+
+                    
+                        //HUMBERTO
+                        const humberto = this.add.image(
+                            this.sys.game.canvas.width / 2,
+                            this.sys.game.canvas.height / 1.4, 
+                            'humberto')
+                        .setOrigin(0.5, 0.5)
+                        .setScale(0.4)
+                        .setInteractive()
+                        .on('pointerdown', () => this.acceptButton(true, arrows, names, acceptButton, backButton, "Quieres hablar con HUMBERTO?"))
+                        .on('pointerover', () => humberto.setTint(0xff0000))
+                        .on('pointerout', () => humberto.clearTint());
+                }
+                else if(this.mode == 'parque') //pruebas
+                    {
+                        
+                        var names = this.add.group();
+
+                        let pacoName = this.add.text(
+                            this.sys.game.canvas.width / 4,   // Coordenada X: centrado horizontalmente
+                            this.sys.game.canvas.height / 3.7, // Coordenada Y: 1/10 del alto de la pantalla 
+                            "PACO", 
+                            { 
+                                fontSize: '100px', 
+                                color: '#999999',       // Negro
+                                fontFamily: 'Georgia',  
+                            }
+                        );
+                        pacoName.setStroke('#000000', 8);  // Trazo negro, puedes ajustar el grosor o eliminarlo
+                        pacoName.setOrigin(0.5, 0);
+                        pacoName.setScale(0.6);
+
+                        names.add(pacoName);
+
+                        let humbertoName = this.add.text(
+                            this.sys.game.canvas.width / 2,   // Coordenada X: centrado horizontalmente
+                            this.sys.game.canvas.height / 3.7, // Coordenada Y: 1/10 del alto de la pantalla 
+                            "HUMBERTO", 
+                            { 
+                                fontSize: '100px', 
+                                color: '#999999',       // Negro
+                                fontFamily: 'Georgia',  
+                            }
+                        );
+                        humbertoName.setStroke('#000000', 8);  // Trazo negro, puedes ajustar el grosor o eliminarlo
+                        humbertoName.setOrigin(0.5, 0);
+                        humbertoName.setScale(0.6);
+
+                        names.add(humbertoName);
+
+                        let mariaName = this.add.text(
+                            this.sys.game.canvas.width / 1.37,   // Coordenada X: centrado horizontalmente
+                            this.sys.game.canvas.height / 3.7, // Coordenada Y: 1/10 del alto de la pantalla 
+                            "MARIA", 
+                            { 
+                                fontSize: '100px', 
+                                color: '#999999',       // Negro
+                                fontFamily: 'Georgia',  
+                            }
+                        );
+                        mariaName.setStroke('#000000', 8);  // Trazo negro, puedes ajustar el grosor o eliminarlo
+                        mariaName.setOrigin(0.5, 0);
+                        mariaName.setScale(0.6);
+
+                        names.add(mariaName);
+                        
+                        
+                        
+                        
+                        
+                        
+                        //PACO
+                        const paco = this.add.image(
+                            this.sys.game.canvas.width / 4,
+                            this.sys.game.canvas.height / 1.4, 
+                            'paco')
+                        .setOrigin(0.5, 0.5)
+                        .setScale(0.9)
+                        .setInteractive()
+                        .on('pointerdown', () => this.acceptButton(true, arrows, names, acceptButton, backButton, "Quieres hablar con PACO?"))
+                        .on('pointerover', () => paco.setTint(0xff0000))
+                        .on('pointerout', () => paco.clearTint());
+                        
+
+
+                        
+
+
+                        //HUMBERTO
+                        const humberto = this.add.image(
+                            this.sys.game.canvas.width / 2,
+                            this.sys.game.canvas.height / 1.4, 
+                            'humberto')
+                        .setOrigin(0.5, 0.5)
+                        .setScale(0.4)
+                        .setInteractive()
+                        .on('pointerdown', () => this.acceptButton(true, arrows, names, acceptButton, backButton, "Quieres hablar con HUMBERTO?"))
+                        .on('pointerover', () => humberto.setTint(0xff0000))
+                        .on('pointerout', () => humberto.clearTint());
+
+
+                        
+
+                        //MARIA
+                        const maria = this.add.image(
+                            this.sys.game.canvas.width / 1.35,
+                            this.sys.game.canvas.height / 1.4, 
+                            'maria')
+                        .setOrigin(0.5, 0.5)
+                        .setScale(0.6)
+                        .setInteractive()
+                        .on('pointerdown', () => this.acceptButton(true, arrows, names, acceptButton, backButton, "Quieres hablar con MARIA?"))
+                        .on('pointerover', () => maria.setTint(0xff0000))
+                        .on('pointerout', () => maria.clearTint());
+
+                        
+                    }
+
+
+
             //FLECHAS
                 var arrows = this.add.group();
                 var arrow1 = this.add.image(
                     this.sys.game.canvas.width / 4,
-                    this.sys.game.canvas.height / 3.2, 
+                    this.sys.game.canvas.height / 2.5, 
                     'arrow')
                     .setScale(0.2, 0.1);
                 arrows.add(arrow1);
 
                 var arrow2 = this.add.image(
                     this.sys.game.canvas.width / 2,
-                    this.sys.game.canvas.height / 3.2, 
+                    this.sys.game.canvas.height / 2.5, 
                     'arrow')
                     .setScale(0.2, 0.1);
                 arrows.add(arrow2);
+
+                var arrow3 = this.add.image(
+                    this.sys.game.canvas.width / 1.37,
+                    this.sys.game.canvas.height / 2.5, 
+                    'arrow')
+                    .setScale(0.2, 0.1);
+                arrows.add(arrow3);
 
 
             //ACCEPT && BACK
@@ -115,64 +331,15 @@ export default class localizationScene extends Phaser.Scene
                 .setScale(-0.3, 0.3)
                 .setInteractive()
                 .on('pointerdown', () => {
-                    this.acceptButton(false, arrows, acceptButton, backButton);
+                    this.acceptButton(false, arrows, names, acceptButton, backButton, "Con quien quieres hablar?");
                 });
     ;
                 
-                this.acceptButton(false, arrows, acceptButton, backButton); //empieza ocultando boton aceptar
+                this.acceptButton(false, arrows, names, acceptButton, backButton, "Con quien quieres hablar?"); //empieza ocultando boton aceptar
 
 
             
 
-            
-
-            //NPCS (DEPENDEN DE DATA)
-                console.log("mirar data");
-                this.mode = data.modo || 2;
-                console.log(this.mode);
-                
-                if(this.mode == 1)
-                {
-                    //PACO
-                    const paco = this.add.image(
-                        this.sys.game.canvas.width / 4,
-                        this.sys.game.canvas.height / 1.5, 
-                        'paco')
-                    .setOrigin(0.5, 0.5)
-                    .setInteractive()
-                    .on('pointerdown', () => this.acceptButton(true, arrows, acceptButton, backButton))
-                    .on('pointerover', () => paco.setTint(0xff0000))
-                    .on('pointerout', () => paco.clearTint());
-
-                    //HUMBERTO
-                    const humberto = this.add.image(
-                        this.sys.game.canvas.width / 2,
-                        this.sys.game.canvas.height / 1.5, 
-                        'humberto')
-                    .setOrigin(0.5, 0.5)
-                    .setScale(0.45)
-                    .setInteractive()
-                    .on('pointerdown', () => this.acceptButton(true, arrows, acceptButton, backButton))
-                    .on('pointerover', () => humberto.setTint(0xff0000))
-                    .on('pointerout', () => humberto.clearTint());
-                }
-                else if(this.mode == 2)
-                {
-                    //MARIA
-                    const maria = this.add.image(
-                        this.sys.game.canvas.width / 1.96,
-                        this.sys.game.canvas.height / 1.5, 
-                        'maria')
-                    .setOrigin(0.5, 0.5)
-                    .setScale(0.7)
-                    .setInteractive()
-                    .on('pointerdown', () => this.acceptButton(true, arrows, acceptButton, backButton))
-                    .on('pointerover', () => maria.setTint(0xff0000))
-                    .on('pointerout', () => maria.clearTint());
-                }
-                
-
-                
 
 
             //BACK BUTTON
@@ -201,7 +368,7 @@ export default class localizationScene extends Phaser.Scene
 
     }
 
-    acceptButton(show, arrows, acceptButton, backButton) //para enseñar y ocultar botones
+    acceptButton(show, arrows, names, acceptButton, backButton, nuevoTexto) //para enseñar y ocultar botones
     {
         if(show == true)
         {
@@ -212,6 +379,13 @@ export default class localizationScene extends Phaser.Scene
 
             //ocultar flechas
             arrows.setVisible(false);
+
+            //ocultar names
+            names.setVisible(false);
+
+            if (this.titulo) {
+                this.titulo.setText(nuevoTexto);  // Cambia el texto en el objeto titulo
+            }
         }
         else
         {
@@ -221,6 +395,13 @@ export default class localizationScene extends Phaser.Scene
 
             // Mostrar flechas
             arrows.setVisible(true);
+
+            //mostrar names
+            names.setVisible(true);
+
+            if (this.titulo) {
+                this.titulo.setText(nuevoTexto);  // Cambia el texto en el objeto titulo
+            }
         }
          
     }
