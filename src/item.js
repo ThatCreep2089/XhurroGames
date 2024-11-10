262f3dad7dddcf66719227f637c3ad4ca36afe08
Nerea E. <nereaesp@ucm.es>
2024-11-10 14:37:19 +0100

    Reapply "animaciones bien"
    This reverts commit fa30ed60b6a5f0163fd2ab86c6b359102410e19f.

--- src/item.js
+++ src/item.js
@@ -6,35 +6,38 @@ constructor(scene,name,description,effect,posx,posy,amount)
 {
     super(scene,posx,posy,name);
         this.amount = amount; // elle
-        this.name=name;
-        this.description=description;
-        this.effect=effect;
+        this.name=name; //nombre elem
+        this.description=description;//que hace el objeto
+        this.effect=effect;//tipo efecto(si es para reducir ansiedad...)
         this.scene.add.existing(this); //Añadimos item a la escena
-      this.ejemplar=1;//cauntos ejemplares del mismo elem hay
+        this.ejemplar=1;//cauntos ejemplares del mismo elem hay
 
 }
 
 
 
-
+//curar vida
 HealLife(num,player)
 {
 player.HealPlayer(num);
 console.log("vida +"+ this.amount+num);
 }
 
+//incrementar vida max
 IncreaseLifeMax(num,player)
 {
     player.MaxLife(amount)
     console.log("vidamax +"+this.amount+num);
 }
 
+//reducir ansiedad
 ReduceAnxiety(num,player)
 {
     player.LessAnxiety(amount)
     console.log("ansiedad -" +this.amount+num);
 }
 
+//cuara ptos cualidad
 HealCuality(num,player)
 {
     player.HealQuality(amount)
