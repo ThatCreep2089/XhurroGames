3190ae7c9a18284be20c0a5a09627f0e7257fd76
Nerea E. <nereaesp@ucm.es>
2025-01-19 13:58:35 +0100

    cambiado stats

--- scenes/StatsScene.js
+++ scenes/StatsScene.js
@@ -21,7 +21,13 @@ init(data){
     this.dialogueJson = data.dialogueJson;
     
 }
-
+CrearText(x,y,string,fontSize,color,num)
+{
+ let text=this.add.text(x,y,string)
+ text.setFontSize(fontSize);
+ text.setStroke(color, num);
+ return text;
+}
     create() {
        //Creamos el player para poder acceder a el 
        this.player= new Player(this,0,0);
@@ -51,7 +57,6 @@ init(data){
         //Pintamos un fondo
         let fondo=this.add.image(0, 0, 'fondoStats').setOrigin(0, 0);
 
-    
         //añadimos rectangulo
         let rect = this.add.rectangle(0, 200,2900, 800, 0xf6f6f6).setOrigin(0);//fondo
         
@@ -59,60 +64,37 @@ init(data){
         let elleRojo= this.add.image(300,600,'elleRojo','assets/stats/elleRojo.png'); 
         elleRojo.setScale(0.8,0.8);
 
-        let elleVerde= this.add.image(1200,600,'elleVerde','assets/stats/elleVerde.png'); 
-        elleVerde.setScale(0.8,0.8);
-
-        let elleAmarillo= this.add.image(1650,600,'elleAmarillo','assets/stats/elleAmarillo.png'); 
-        elleAmarillo.setScale(0.8,0.8);
-
-                
-        let elleAzul= this.add.image(750,600,'elleAzul','assets/stats/elleAzul.png'); 
-        elleAzul.setScale(0.8,0.8);
-                
+      
         // TITULOS ESTADISTICAS
-        let titulo = this.add.text(0, this.sys.game.canvas.height/15,
-        "ESTADISTICAS");
-        titulo.setFontSize(100);
-        titulo.setStroke('#000000', 7);
-        titulo.x = this.sys.game.canvas.width/2 - titulo.width/2; 
+        let titulo = this.CrearText(600, this.sys.game.canvas.height/15,
+        "ESTADÍSTICAS",100,'#000000',7);
+      
+        let vida= this.CrearText(550, 500,
+            "VIDA"+" "+this.player.health ,60,'#000000',7);
+       
+        let vidaM= this.CrearText(500, 600,
+            "VIDA MÁX"+" "+this.player.maxHealth ,60,'#000000',7);
+   
+        let ansi= this.CrearText(550, 400,
+            "ANSIEDAD"+" "+this.player.ansiedad,60,'#000000',7);
+ 
+        let cualidades= this.CrearText(1000, 400,
+            "CUALIDADES",60,'#000000',7);
+ 
 
-        let vida= this.add.text(175, 500,
-        "VIDA"+" "+this.player.health );
-        vida.setFontSize(60);
-        vida.setStroke('#000000', 7);
-        let vidaM= this.add.text(100, 600,
-        "VIDA MÁX"+" "+this.player.maxHealth);
-        vidaM.setFontSize(60);
-        vidaM.setStroke('#000000', 7);
-           
-        let ansi= this.add.text(550, 500,
-        "ANSIEDAD"+" "+this.player.ansiedad);
-        ansi.setFontSize(60);
-        ansi.setStroke('#000000', 7);
-                
-        let cualidades= this.add.text(1000, 400,
-        "CUALIDADES");
-        cualidades.setFontSize(60);
-        cualidades.setStroke('#000000', 7);
-
-        let humildadd= this.add.text(1000, 500,
-            "HUMILDAD"+" "+this.player.humildad);
-            humildadd.setFontSize(50);
-            humildadd.setStroke('#000000', 7); 
+        let humildadd= this.CrearText(1000, 500,
+            "HUMILDAD"+" "+this.player.humildad,50,'#000000',7);
+ 
          
-            
-            let trabajoo= this.add.text(1000, 600,
-                "TRABAJO DURO"+" "+this.player.trabajoDuro);
-                trabajoo.setFontSize(50);
-                trabajoo.setStroke('#000000', 7); 
-                let Afecto= this.add.text(1000, 700,
-                    "AFECTO"+" "+this.player.afecto);
-                    Afecto.setFontSize(50);
-                    Afecto.setStroke('#000000', 7);  
-                    let Agnosticismo= this.add.text(1000, 800,
-                        "AGNOSTICISMO"+" "+this.player.agnosticismo);
-                        Agnosticismo.setFontSize(50);
-                        Agnosticismo.setStroke('#000000', 7);     
+            let trabajoo= this.CrearText(1000, 600,
+                "TRABAJO DURO"+" "+this.player.trabajoDuro,50,'#000000',7);
+           
+                let Afecto= this.CrearText(1000, 700,
+                    "AFECTO"+" "+this.player.afecto,50,'#000000',7);
+             
+                    let Agnosticismo= this.CrearText(1000, 800,
+                        "AGNOSTICISMO"+" "+this.player.agnosticismo,50,'#000000',7);
+                       
         let ptos= this.add.text(1575, 450,
         "PTOS");
         ptos.setFontSize(60);
