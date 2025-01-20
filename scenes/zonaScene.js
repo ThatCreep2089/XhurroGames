776416bc6b27891710916ec2caf63b605ca2b636
Andrea G.A. <149385951+andreaGonzAnas@users.noreply.github.com>
2025-01-20 11:04:25 +0100

    zona 1 porras
    faltan colliders

--- scenes/zonaScene.js
+++ scenes/zonaScene.js
@@ -24,7 +24,7 @@ export default class ZonaScene extends Phaser.Scene{
         
         this.key="zonaScene";
         
-        this.modo= data.modo || 1;
+        this.modo= data.modo || 8;
         this.fondo = 'fondo' + this.modo;
         
         
