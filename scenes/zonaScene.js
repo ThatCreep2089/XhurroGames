2e3c7ce4639648a5b9aa02b9f5143712ad4b43e6
Andrea G.A. <149385951+andreaGonzAnas@users.noreply.github.com>
2025-01-21 12:28:58 +0100

    seteando dialogos calderilla
    ya no peta nada en calderilla (creo)

--- scenes/zonaScene.js
+++ scenes/zonaScene.js
@@ -24,7 +24,7 @@ export default class ZonaScene extends Phaser.Scene{
         
         this.key="zonaScene";
         
-        this.modo= data.modo || 7;
+        this.modo= data.modo || 6;
         this.fondo = 'fondo' + this.modo;
         
         
